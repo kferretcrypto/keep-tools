5b8cc079afc4f76e58ff9a78318a3c1479f62f00
kferret <kferret@protonmail.com>
2020-09-27 21:37:58 -0700

    Add mainnet addresses to AddressList

--- src/components/pages/AddressList.js
+++ src/components/pages/AddressList.js
@@ -4,6 +4,48 @@ import { H1, H2 } from '../common/H'
 import Table from '../common/Table'
 import A from '../common/A'
 
+// Mainnet
+
+const mainnetTokenAddresses = [
+  ['KEEP', '0x85eee30c52b0b379b046fb0f85f4f3dc3009afec'],
+  ['tBTC', '0x8dAEBADE922dF735c38C80C7eBD708Af50815fAa'],
+  ['tBTC Deposit Token (TDT)', '0x10B66Bd1e3b5a936B7f8Dbc5976004311037Cdf0'],
+]
+
+const mainnetTokenTableData = [['Name', 'Address']].concat(
+  mainnetTokenAddresses.map((record) => [
+    record[0],
+    <A href={`https://etherscan.io/address/${record[1]}`}>{record[1]}</A>,
+  ])
+)
+
+const mainnetBeaconAddresses = [
+  ['TokenStaking', '0x1293a54e160d1cd7075487898d65266081a15458'],
+  ['KeepRandomBeaconService', '0x50510e691c90ea098e3fdd23c311731bf394aafd'],
+  ['KeepRandomBeaconOperator', '0xdf708431162ba247ddae362d2c919e0fbafcf9de'],
+]
+
+const mainnetBeaconTableData = [['Name', 'Address']].concat(
+  mainnetBeaconAddresses.map((record) => [
+    record[0],
+    <A href={`https://etherscan.io/address/${record[1]}`}>{record[1]}</A>,
+  ])
+)
+
+const mainnetECDSAAddresses = [
+  ['BondedECDSAKeepFactory', '0xA7d9E842EFB252389d613dA88EDa3731512e40bD'],
+  ['Sanctioned Applications', '0xe20A5C79b39bC8C363f0f49ADcFa82C2a01ab64a '],
+]
+
+const mainnetECDSATableData = [['Name', 'Address']].concat(
+  mainnetECDSAAddresses.map((record) => [
+    record[0],
+    <A href={`https://etherscan.io/address/${record[1]}`}>{record[1]}</A>,
+  ])
+)
+
+// Ropsten
+
 const ropstenTokenAddresses = [
   ['KEEP', '0x343d3DDA00415289CDD4E8030F63a4A5a2548ff9'],
   ['tBTC', '0x7c07C42973047223F80C4A69Bb62D5195460Eb5F'],
@@ -51,6 +93,12 @@ const ropstenECDSATableData = [['Name', 'Address']].concat(
 const AddressList = () => (
   <React.Fragment>
     <H1>Address List</H1>
+    <H2>Mainnet - Tokens</H2>
+    <Table data={mainnetTokenTableData} />
+    <H2>Mainnet - Random Beacon Node</H2>
+    <Table data={mainnetBeaconTableData} />
+    <H2>Mainnet - ECDSA Node</H2>
+    <Table data={mainnetECDSATableData} />
     <H2>Ropsten - Tokens</H2>
     <Table data={ropstenTokenTableData} />
     <H2>Ropsten - Random Beacon Node</H2>
