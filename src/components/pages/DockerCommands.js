da98d396c2be879861e8bee532c3d1cd2ff1c17b
kferret <kferret@protonmail.com>
2020-10-15 09:31:54 -0700

    Update keep-ecdsa-client to v1.4.0 in DockerCommands

--- src/components/pages/DockerCommands.js
+++ src/components/pages/DockerCommands.js
@@ -79,7 +79,7 @@ const RUN_ECDSA_CMD = (v) =>
 --log-opt max-file=3 \\
 --name ecdsa-node \\
 -p 3919:3919 \\
-keepnetwork/keep-ecdsa-client:v1.3.0 --config /mnt/keep-ecdsa-client/config/config.toml start`
+keepnetwork/keep-ecdsa-client:v1.4.0 --config /mnt/keep-ecdsa-client/config/config.toml start`
 
 const PS_CMD = () => `docker ps`
 
